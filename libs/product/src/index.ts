8f0b59c610c34088f36898fc8d3e182a9706007f
Johnny Pestana <johnnygpestana@gmail.com>
2023-07-27 22:15:30 -0300

    lib:product | added

---
+++ libs/product/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/product.module';
+export * from './lib/product.service';
+export * from './lib/product.interface';
---
+++ libs/product/src/lib/product.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products: { [key: number]: Product } = {};
+
+  getById(id: number) {
+
+    if (this.products[id]) {
+      this.loadProduct(id);
+    }
+
+    return this.products[id];
+  }
+
+  private loadProduct(id: number): void {
+    this.products[id] = {
+      id: id,
+      name: 'Tenis Nike Air Jordan',
+      description: 'Tenis de corrida para uso casual',
+      price: 899.99
+    };
+  }
+  
+}
