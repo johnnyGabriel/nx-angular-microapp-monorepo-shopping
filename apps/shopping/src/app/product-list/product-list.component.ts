e6f50e2d1a9b35e762ac7a84e3593e6b9e62e2a0
Johnny Pestana <johnnygpestana@gmail.com>
2023-08-02 18:11:30 -0300

    shopping: populating product list using StoreWindow

--- apps/shopping/src/app/product-list/product-list.component.ts
+++ apps/shopping/src/app/product-list/product-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { StoreWindowService } from '../store-window.service';
 
 @Component({
   selector: 'shopping-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+
+  products$ = this.storeWindow.get();
+
+  constructor(private storeWindow: StoreWindowService) {}
+  
+  handleAddToCart(productId: number) {
+    console.log(`product ${productId} added to cart`);
+  }
+  
+}
