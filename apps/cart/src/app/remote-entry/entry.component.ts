ebdac59ad582b5c601a99bb52bb2eacd3e151277
Johnny Pestana <johnnygpestana@gmail.com>
2023-08-02 18:49:09 -0300

    remote:cart | created

---
+++ apps/cart/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'microapp-monorepo-shopping-cart-entry',
+  template: `<microapp-monorepo-shopping-nx-welcome></microapp-monorepo-shopping-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
--- apps/shopping/src/app/app.routes.ts
+++ apps/shopping/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Route } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
+  {
+    path: 'cart',
+    loadChildren: () =>
+      loadRemoteModule('cart', './Module').then((m) => m.RemoteEntryModule),
+  },
   {
     path: '',
     component: ProductListComponent,
